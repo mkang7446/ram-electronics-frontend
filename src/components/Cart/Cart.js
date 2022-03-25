0927a5ed7dcde468be4456d650913fb65843a854
acourney <acourney@ucsc.edu>
2022-03-24 17:20:42 -0700

    somewhat fix cart layout and succesfully delete item from cart without deleting cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-import './Cart.css';
+import "./Cart.css";
 
 function Cart(props) {
   const [cart, setCart] = useState([]);
   const [cartEmpty, setCartEmpty] = useState(true);
   const navigate = useNavigate();
 
-  const url = 'https://thawing-mountain-85716.herokuapp.com/api/carts';
+  const url =
+    "https://thawing-mountain-85716.herokuapp.com/api/carts/623cfceabf21be8483a4b201";
 
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        setCart(data);
+        console.log(data);
+        console.log(data.item);
+        setCart(data.item);
 
-        if (data.length > 0) {
+        if (data.item.length > 0) {
           setCartEmpty(false);
         }
       })
-      .catch((err) => console.log('oops error'));
+      .catch((err) => console.log("oops error"));
   }, []);
 
   // const handleRemove = (event) => {
@@ -41,13 +44,13 @@ function Cart(props) {
 
   const handleRemove = async (event) => {
     // Write your DELETE fetch() or axios() request here
-    const confirm = window.confirm('Are you sure you want to delete?');
+    const confirm = window.confirm("Are you sure you want to delete?");
     if (confirm) {
       try {
         const response = await axios.delete(`${url}/${event.currentTarget.id}`);
         // if operation was successful navigate back to home
         if (response.status === 200) {
-          console.log('status => 200');
+          console.log("status => 200");
         }
       } catch (error) {
         // more error handling
@@ -63,70 +66,33 @@ function Cart(props) {
   // };
 
   return (
-<<<<<<< HEAD
     <div className="cart">
       <div className="cart-grid">
-        {cart.map((itemToBuy) => {
-          console.log(itemToBuy.item[0].name);
-          <>
-            <div className="cart-item-image">
-              <img src={itemToBuy.image} alt="" />
-            </div>
-            <div className="item-price-in-cart">
-              {itemToBuy.item[0]["price"]}
-            </div>
-            <div className="item-cart-quantity">quantity dropdown here</div>
-            <div className="remove-from-cart">
-              <button onClick="handleRemove">Remove Item</button>
-            </div>
-          </>;
-        })}
-        {console.log(cart)}
-        {/* {!cartEmpty ? (
-          <>
-            {" "}
-            <div className="cart-item-image">
-              <img src={cart[0].item[0].image} alt="" />
-            </div>
-            <div className="item-price-in-cart">{cart[0].item[0].price}</div>
-            <div className="item-cart-quantity">quantity dropdown here</div>
-            <div className="remove-from-cart">
-              <button onClick={handleRemove} id={cart[0].item[0]["_id"]}>
-                Remove Item
-              </button>
-            </div>{" "}
-          </>
-        ) : (
-          <p>Your Cart Is Empty!</p>
-        )} */}
-=======
-    <div className='cart'>
-      <div className='cart-grid'>
         {/* {cartList()} */}
-        {cart.map((itemToBuy, idx) => {
-          console.log(itemToBuy);
-          return (
-            <div className='cart-mapping' key={idx}>
-              <div className='cart-item-image'>
-                <img src={itemToBuy.item[0].image} alt='' />
+        {!cartEmpty ? (
+          cart.map((itemToBuy, idx) => {
+            console.log(itemToBuy);
+            return (
+              <div className="cart-mapping" key={idx}>
+                <div className="cart-item-image">
+                  <img src={itemToBuy.image} alt="" />
+                </div>
+                <div className="item-price-in-cart">{itemToBuy.price}</div>
+                <div className="item-cart-quantity">Limit 1 per customer</div>
+                <div className="remove-from-cart">
+                  <button onClick={handleRemove} id={itemToBuy["_id"]}>
+                    Remove Item
+                  </button>
+                </div>
               </div>
-              <div className='item-price-in-cart'>
-                {itemToBuy.item[0].price}
-              </div>
-              <div className='item-cart-quantity'>quantity dropdown here</div>
-              <div className='remove-from-cart'>
-                <button onClick={handleRemove} id={itemToBuy['_id']}>
-                  Remove Item
-                </button>
-              </div>
-            </div>
-          );
-        })}
-        {console.log(cart)}
->>>>>>> 8c9ec6d3e15574c5de3af83996a2f8d0c6df3b81
+            );
+          })
+        ) : (
+          <p>Your cart is empty!</p>
+        )}
       </div>
 
-      <footer className='cart-total'>
+      <footer className="cart-total">
         <p>
           Total: <span>price sum here</span>
         </p>
